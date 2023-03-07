a447c609528732b5019ae9aaef772c4713ad44f4
Julio Soares Mendes <julio_soares_mendes@hotmail.com>
2023-03-07 00:08:49 -0300

    feat: move sutCreation to a factory helper

--- src/presentation/routes/login-routes.spec.js
+++ src/presentation/routes/login-routes.spec.js
@@ -1,9 +1,13 @@
 const LoginRouter = require("./login-router");
 const MissingParamError = require("../helpers/missing-param-error");
 
+const makeSut = () => {
+   return new LoginRouter();
+};
+
 describe("Login Router", () => {
    test("Should return 400 if no email is provided", () => {
-      const sut = new LoginRouter();
+      const sut = makeSut();
       const httpResquest = {
          body: {
             password: "any_password",
@@ -14,7 +18,7 @@ describe("Login Router", () => {
       expect(httpResponse.body).toEqual(new MissingParamError("email"));
    });
    test("Should return 400 if no password is provided", () => {
-      const sut = new LoginRouter();
+      const sut = makeSut();
       const httpResquest = {
          body: {
             email: "any_email@mail.com",
@@ -26,13 +30,13 @@ describe("Login Router", () => {
    });
 
    test("should return 500 if no httpRequest is provided", () => {
-      const sut = new LoginRouter();
+      const sut = makeSut();
       const httpResponse = sut.route();
       expect(httpResponse.statusCode).toBe(500);
    });
 
    test("should return 500 if no httpRequest has no body", () => {
-      const sut = new LoginRouter();
+      const sut = makeSut();
       const httpResponse = sut.route({});
       expect(httpResponse.statusCode).toBe(500);
    });
