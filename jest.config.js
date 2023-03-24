831b4644a7a24712d9c33ab94b330dbc29e1f60d
Julio Soares Mendes <julio_soares_mendes@hotmail.com>
2023-03-24 18:04:05 -0300

    teste lint

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
-   collectCoverage: true,
-   coverageDirectory: "coverage",
-   coverageProvider: "v8",
-};
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['**/src/**/*.js']
+}
