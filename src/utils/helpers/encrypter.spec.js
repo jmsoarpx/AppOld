0c283c6588698a18b98f5beb7f67e2844c1357fd
Julio Mendes Tonon <julio.mendes@mobilemed.com.br>
2023-04-05 23:54:37 -0300

    ensure Encrypter returns false if bcrypt returns false

---
+++ src/utils/helpers/encrypter.spec.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt')
+
+class Encrypter {
+  async compare (value, hash) {
+    const isValid = bcrypt.compare(value, hash)
+    return isValid
+  }
+}
+
+describe('Encrypter', () => {
+  test('Should return true if bcrypt returns true', async () => {
+    const sut = new Encrypter()
+    const isValid = await sut.compare('any_value', 'hased_value')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should return false if bcrypt returns false', async () => {
+    const sut = new Encrypter()
+    bcrypt.isValid = false
+    const isValid = await sut.compare('any_value', 'hased_value')
+    expect(isValid).toBe(false)
+  })
+})
