9fdc46c65bee8550eadab7db31fc35b86042a20f
Julio Mendes Tonon <julio.mendes@mobilemed.com.br>
2023-04-07 02:28:52 -0300

    ensure LoadUserByEmailRepository returns an user with id and password

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,13 +1,28 @@
 const { MongoClient } = require('mongodb')
 
 let client, db
+const mockUser = {
+  _id: 'any_id',
+  email: 'valid_email@mail.com',
+  name: 'any_name',
+  idade: 35,
+  state: 'any_state',
+  password: 'hashed_password'
+}
 class LoadUserByEmailRepository {
   constructor (userModel) {
     this.userModel = userModel
   }
 
   async load (email) {
-    const user = await this.userModel.findOne({ email })
+    const user = await this.userModel.findOne(
+      { email },
+      {
+        projection: {
+          password: 1
+        }
+      }
+    )
     return user
   }
 }
@@ -46,10 +61,11 @@ describe('LoadUserByEmail Repository', () => {
 
   test('Should return an user if user is found', async () => {
     const { sut, userModel } = makeSut()
-    await userModel.insertOne({
-      email: 'valid_email@mail.com'
-    })
+    await userModel.insertOne(mockUser)
     const user = await sut.load('valid_email@mail.com')
-    expect(user.email).toBe('valid_email@mail.com')
+    expect(user).toEqual({
+      _id: mockUser._id,
+      password: mockUser.password
+    })
   })
 })
