f83c90fa44af81723ad4ac4340b3e3a18949f3de
Julio Mendes Tonon <julio.mendes@mobilemed.com.br>
2023-04-13 00:30:19 -0300

    ensure requests return given content-type when forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -10,4 +10,14 @@ describe('Content-type Middleware', () => {
       .get('/teste-content-type')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content-type is forced', async () => {
+    app.get('/teste-content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/teste-content-type-xml')
+      .expect('content-type', /xml/)
+  })
 })
